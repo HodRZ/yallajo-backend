14482dc32cf03f333f9b2ef723335942672d5ccc
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-08-13 16:26:46 +0300

    Add City Search handler

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-'use strict'
-require('dotenv').config()
-const express = require("express")
-const app = express()
-const cors = require('cors');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 app.use(cors());
-app.use(express.json())
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://backlog:9Wv0YEAqVwKIzJ5K@cluster0.ne4hply.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("CONNECTION OPEN!!!")
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!!!!")
-        console.log(err)
-    })
+app.use(express.json());
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_PORT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO ERROR!!!!");
+    console.log(err);
+  });
+
+//// by Eyad branch name city search backend
+
+const { handleApi } = require("./modules/testLocation");
+app.get("/city", handleApi);
+
+/// by Eyad branch name city search backend
 
 app.listen(3001, () => {
-    console.log('welcome to radio 3001')
-})
+  console.log("welcome to radio 3001");
+});
