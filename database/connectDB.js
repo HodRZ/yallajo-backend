fa005e991864551ad305ac593c6d732a8fc003ba
eyad-mansour <eyad.mansour.1997@gmail.com>
2022-08-14 02:38:36 +0300

    Add find user by id

--- database/connectDB.js
+++ database/connectDB.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_PORT, {
+  .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
